332caa046fe68971a846c2197c9526133f802b55
Harry Randazzo <harry.razzle.randazzo@pm.me>
2021-12-09 11:15:40 -0600

    more cleanup + comment style changes

--- dragndrop_template.ps1
+++ dragndrop_template.ps1
@@ -3,7 +3,7 @@ function main {
     [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
     [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
 
-    ### Create form ###
+    # initial form creation
     $form = New-Object System.Windows.Forms.Form
     $form.Text = "Template Drag n Drop"
     $form.Size = New-Object System.Drawing.Size(475, 195)
@@ -20,8 +20,7 @@ function main {
     $stream.Write($iconBytes, 0, $iconBytes.Length);
     $form.Icon = [System.Drawing.Icon]::FromHandle((New-Object System.Drawing.Bitmap -Argument $stream).GetHIcon())
 
-    ### Define controls ###
-
+    # start button
     $button = New-Object System.Windows.Forms.Button
     $button.Left = 320
     $button.Top = 30
@@ -30,30 +29,31 @@ function main {
     $button.Text = "START"
     $button.BackColor = [System.Drawing.ColorTranslator]::FromHtml("#111622")
 
+    # instructions label
     $label = New-Object Windows.Forms.Label
     $label.Left = 10
     $label.Top = 5
     $label.AutoSize = $True
     $label.Text = "Drop network template files here, config files will output in same directory."
 
+    # files listbox
     $listBox = New-Object Windows.Forms.ListBox
     $listBox.Left = 15
     $listBox.Top = 30
     $listBox.Height = 100
     $listBox.HorizontalScrollbar = $True
     $listBox.Width = 300
-    # $listBox.Width = 505
     $listBox.Anchor = ([System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Left -bor [System.Windows.Forms.AnchorStyles]::Right -bor [System.Windows.Forms.AnchorStyles]::Top)
     $listBox.IntegralHeight = $False
     $listBox.AllowDrop = $True
     $listBox.BackColor = [System.Drawing.ColorTranslator]::FromHtml("#111622")
     $listBox.ForeColor = [System.Drawing.ColorTranslator]::FromHtml("#DCDCDC")
 
+    # progress bar
     $statusBar = New-Object System.Windows.Forms.StatusBar
     $statusBar.Text = "Ready"
 
-    ### Add controls to form ###
-
+    # add controls to form
     $form.SuspendLayout()
     $form.Controls.Add($button)
     $form.Controls.Add($label)
@@ -61,10 +61,8 @@ function main {
     $form.Controls.Add($statusBar)
     $form.ResumeLayout()
 
-    ### Write event handlers ###
-
+    # "START" button click event
     $button_Click = {
-
         foreach ($item in $listBox.Items) {
             $file = Get-Item -LiteralPath $item
             $folder = ($file).Directory.FullName
@@ -72,9 +70,7 @@ function main {
 
             if ($file -is [System.IO.DirectoryInfo]) {
                 Write-Host ("ERROR DIRECTORY")
-            }
-
-            else {
+            } else {
                 # $configfile = $folder + "\" + $i.BaseName + $i.Extension
                 # Write-Host $configfile
                 $titles = ((Get-Content ($folder + "\variables.csv"))[0] -split ',')
@@ -106,17 +102,14 @@ function main {
 
                     $form_FormClosed
                     $form.Close()
-
                 }
-
             }
-
         }
     }
 
+    # list box dragover event
     $listBox_DragOver = [System.Windows.Forms.DragEventHandler] {
         if ($_.Data.GetDataPresent([Windows.Forms.DataFormats]::FileDrop)) {
-            # $_ = [System.Windows.Forms.DragEventArgs]
             $_.Effect = 'Copy'
         }
         else {
@@ -124,13 +117,14 @@ function main {
         }
     }
 
+    # list box drop event
     $listBox_DragDrop = [System.Windows.Forms.DragEventHandler] {
         foreach ($filename in $_.Data.GetData([Windows.Forms.DataFormats]::FileDrop)) {
-            # $_ = [System.Windows.Forms.DragEventArgs]
             $listBox.Items.Add($filename)
         }
     }
 
+    # form close event, performs cleanup
     $form_FormClosed = {
         try {
             $listBox.remove_Click($button_Click)
@@ -139,13 +133,12 @@ function main {
             $listBox.remove_DragDrop($listBox_DragDrop)
             $form.remove_FormClosed($Form_Cleanup_FormClosed)
         }
-        catch [Exception]
-        { 
+        catch [Exception] { 
             "Exception thrown closing form." 
         }
     }
 
-    # wire up events
+    # wire event listeners to controls
     $button.Add_Click($button_Click)
     $listBox.Add_DragOver($listBox_DragOver)
     $listBox.Add_DragDrop($listBox_DragDrop)
