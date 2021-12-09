8b50bde12b353ba3cfb0530ea20f1e71a118fd6d
Harry Randazzo <harry.razzle.randazzo@pm.me>
2021-12-09 11:43:44 -0600

    rename dragndrop_template -> gui

--- engine.bat
+++
@@ -1,4 +0,0 @@
-if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
-pushd %~dp0
-powershell.exe -noprofile -ExecutionPolicy Bypass -Command  .\dragndrop_template.ps1
-exit
