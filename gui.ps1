879665f3e7b5c29caeb0ad423472f36fa5a26754
Harry Randazzo <harry.razzle.randazzo@pm.me>
2021-12-24 13:59:52 -0500

    reference cli in gui, keep it DRY

--- gui.ps1
+++ gui.ps1
@@ -1,3 +1,5 @@
+. $PSCommandPath.Replace('gui', 'cli')
+
 function main {
     [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
     [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
@@ -63,45 +65,9 @@ function main {
     # "START" button click event
     $button_Click = {
         foreach ($item in $listBox.Items) {
-            $templatefile = Get-Item -LiteralPath $item
-            $folder = ($templatefile).Directory.FullName
-
-            if ($templatefile -is [System.IO.DirectoryInfo]) {
-                Write-Host ("ERROR: DIRECTORY PROVIDED")
-            }
-            else {
-                New-Item -Path $folder + "\configs" -ItemType Directory -Force -ErrorAction SilentlyContinue | Out-Null
-
-                $titles = ((Get-Content ($folder + "\variables.csv"))[0] -split ',')
-                # ignore the first element, which is always VARIABLES
-                # all other elements are the device names
-                $titles = $titles[1..$titles.Length]
-
-                Write-Host "------"
-
-                for ($k = 0; $k -lt $titles.Length; $k++) {
-                    # build the config file for each device in the first row of variables.csv
-                    $configfile = $folder + "\configs\" + $templatefile.BaseName + "_config_" + $titles[$k] + $templatefile.Extension
-                    Copy-Item -Path $templatefile -Destination $configfile -Force
-                    
-                    # grab the metadata variables
-                    $meta = Import-Csv ($folder + "\variables.csv")
-
-                    for ($i = 0; $i -lt $meta.Count; $i++) {
-                        $row = $meta[$i]
-                        $col = $titles[$k]
-                        $tag = "{{" + $meta.VARIABLES[$i] + "}}"
-                        Write-Host ("Replace $tag in") $titles[$k] "with" $row.$col "."
-                        # perform the replacement
-                        ((Get-Content -Path $configfile) -replace [RegEx]::Escape("{{" + $row.VARIABLES + "}}"), ($meta.$col) ) | Set-Content -Path $configfile
-                    }
-
-                    Write-Host "------"
-
-                    $form_FormClosed
-                    $form.Close()
-                }
-            }
+            Invoke-SNT -TemplateFile $item
+            $form_FormClosed
+            $form.Close()
         }
     }
 
